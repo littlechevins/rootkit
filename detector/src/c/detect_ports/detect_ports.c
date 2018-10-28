8de3037cec83d1a3fa527ef88a8f490491740f22
Katerina B <katerina.b-p@hotmail.com>
2018-10-28 21:50:18 +1100

    detection

--- detector/src/c/detect_ports/detect_ports.c
+++ detector/src/c/detect_ports/detect_ports.c
@@ -9,11 +9,6 @@
 #include <sys/socket.h>
 #include <sys/sysproto.h>
 #include <stdlib.h>
-#include <string.h>
-#include <fcntl.h>
-#include <kvm.h>
-#include <limits.h>
-#include <nlist.h>
 
 #include <net/if.h>
 #include <netinet/in.h>
@@ -31,17 +26,25 @@ main(int argc, char *argv[])
 	struct port_finding_args *uap;
 	uap = (struct port_finding_args *)argv[1];
 
+	INP_INFO_WLOCK(&tcbinfo);
+
 	struct inpcb *inpb;
 	/* Iterate through the TCP-based inpcb list. */
 	LIST_FOREACH(inpb, tcbinfo.ipi_listhead, inp_list) {
 		if (inpb->inp_vflag & INP_TIMEWAIT)
 			continue;
 
+			INP_LOCK(inpb);
+
 		/* Do we want to hide this local open port? */
 		if (uap->lport == ntohs(inpb->inp_inc.inc_ie.ie_lport))
 			printf("ALERT! Insecure port detected\n");
 
+			INP_UNLOCK(inpb);
+
 	}
+
+	INP_INFO_WUNLOCK(&tcbinfo);
 
 	exit(0);
 }
